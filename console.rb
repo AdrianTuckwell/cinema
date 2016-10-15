61cc5d77a14d75ee1e2cab92d9943490b21cc9ca
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-10-15 15:01:17 +0100

    added methods to models

--- console.rb
+++ console.rb
@@ -18,10 +18,24 @@ customer3 = Customer.new({'name' => 'Harry', 'funds' => "100" })
 customer3.save() 
 
 
+# ---- create fiml objects --------
+film1 = Film.new({'title' => 'Titanic', 'price' => "10" })
+film1.save()
+film2 = Film.new({'title' => 'Moby Dick', 'price' => "10" })
+film2.save() 
+film3 = Film.new({'title' => 'Big', 'price' => "10" })
+film3.save() 
 
-
-
-
+# ---- create film objects --------
+ticket1 = Ticket.new({ 'customer_id' => customer1.id , 'film_id' => film1.id })
+ticket2 = Ticket.new({ 'customer_id' => customer2.id , 'film_id' => film2.id })
+ticket3 = Ticket.new({ 'customer_id' => customer3.id , 'film_id' => film3.id })
+ticket4 = Ticket.new({ 'customer_id' => customer1.id , 'film_id' => film1.id })
+ticket5 = Ticket.new({ 'customer_id' => customer2.id , 'film_id' => film2.id })
+ticket6 = Ticket.new({ 'customer_id' => customer3.id , 'film_id' => film3.id })
+ticket7 = Ticket.new({ 'customer_id' => customer1.id , 'film_id' => film1.id })
+ticket8 = Ticket.new({ 'customer_id' => customer2.id , 'film_id' => film2.id })
+ticket9 = Ticket.new({ 'customer_id' => customer3.id , 'film_id' => film3.id })
 
 #---- break point to jump into pry ---
 binding.pry
